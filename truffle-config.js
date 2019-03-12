89685895bfa8da65266be01b9d1fb91a3f5da427
Super Dev <superdev810@gmail.com>
2019-03-12 13:05:26 -0400

    update config

--- truffle-config.js
+++ truffle-config.js
@@ -22,7 +22,7 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const infuraKey = "42f32e02a01b4c61987049cd0181403f";
 
 const fs = require('fs');
-const mnemonic = 'human frequent mind hurry gain wheat earth there slight boss essay menu';
+const mnemonic = process.env.ethereum_mnemonic;
 
 module.exports = {
   /**
@@ -67,6 +67,17 @@ module.exports = {
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    mainnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/${infuraKey}`),
+      network_id: 1,       // mainnet's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 0,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
     },
 
     // Useful for private networks
